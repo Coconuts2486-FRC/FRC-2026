6a94c7bdfc43555a3fde86907f8e6838a3c32d82
Coco-Prog-1 <github@team2486.org>
2026-01-16 15:24:13 -0700

    made base flywheel subsystem classes

---
+++ src/main/java/frc/robot/subsystems/flywheel/flywheelIO.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.flywheel;
+
+import frc.robot.util.RBSIIO;
+
+public interface flywheelIO extends RBSIIO{
+
+  public default void flywheelSetVolts(double volts) {}
+
+  public default void flywheelSpeed(double speed) {}
+
+  public default void flywheelStop() {}
+}
