7a9536e2eb66c2975a9733532a52deb52435157b
OscarCDavis <oscarcdavis@gmail.com>
2026-01-30 18:06:50 -0700

    added main can to the can checking

--- src/main/java/frc/robot/subsystems/Driver_Info/can_status.java
+++ src/main/java/frc/robot/subsystems/Driver_Info/can_status.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.Driver_Info;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.subsystems.drive.Module;
 import frc.robot.subsystems.imu.ImuIO;
 import frc.robot.util.VirtualSubsystem;
@@ -8,11 +10,28 @@ import org.littletonrobotics.junction.Logger;
 public class can_status extends VirtualSubsystem {
 
   Boolean driveCAN = false;
+  Boolean mainCAN = false;
+
+  private final TalonFX test = new TalonFX(31);
 
   public void can_status() {}
 
   @Override
   public void periodic() {
+
+    var testStatus =
+        BaseStatusSignal.refreshAll(
+            test.getStatorCurrent(),
+            test.getPosition(),
+            test.getVelocity(),
+            test.getMotorVoltage());
+    System.out.println(testStatus.isOK());
+
+    if (testStatus.isOK()) {
+      mainCAN = true;
+    } else {
+      mainCAN = false;
+    }
 
     // if statement to figure out if the drive CAN network is alive
     if (Module.alive1
@@ -27,6 +46,7 @@ public class can_status extends VirtualSubsystem {
 
     // Logger inputs for each CAN network
     Logger.recordOutput("CAN/DriveCAN", driveCAN);
+    Logger.recordOutput("CAN/MainCAN", mainCAN);
 
     // Logger inputs for each part of the drive CAN network
     Logger.recordOutput("CAN/PigeonALive", ImuIO.ImuIOInputs.connected);
@@ -34,5 +54,8 @@ public class can_status extends VirtualSubsystem {
     Logger.recordOutput("CAN/Module2Alive", Module.alive2);
     Logger.recordOutput("CAN/Module3Alive", Module.alive3);
     Logger.recordOutput("CAN/Module4Alive", Module.alive4);
+
+    // logger inputs for each part of the main CAN network
+    Logger.recordOutput("CAN/TestAlive", testStatus.isOK());
   }
 }
