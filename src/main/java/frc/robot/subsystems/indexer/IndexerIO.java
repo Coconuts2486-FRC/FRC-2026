dbcf374bb91203268beeeed68f552f277d211071
OscarCDavis <oscarcdavis@gmail.com>
2026-02-06 19:34:53 -0700

    made talonfx class and auto logging and fixed everything

--- src/main/java/frc/robot/subsystems/indexer/IndexerIO.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIO.java
@@ -1,22 +1,32 @@
 package frc.robot.subsystems.indexer;
 
+import frc.robot.subsystems.flywheel_example.FlywheelIO.FlywheelIOInputs;
 import frc.robot.util.RBSIIO;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface IndexerIO extends RBSIIO {
 
   @AutoLog
-  public static class CoralScorerIOInputs {
+  public static class IndexerIOInputs {
     public double velocityRadPerSec = 0.0;
     public double appliedVolts = 0.0;
     public double[] currentAmps = new double[] {};
+
+    public boolean feederConnected = false;
   }
 
-  public default void indexerSetVolts(double volts) {}
+  public default void feederSetVelocity(double velocity) {}
+
+  public default void feederStop() {}
+
+  public default double getFeederspeed() {
+    return 0.0;
+  }
+
+  public default void updateInputs(IndexerIOInputs inputs) {}
+
 
   public default void indexerSetVelocity(double velocity) {}
-
-  public default void indexerSetPercent(double percent) {}
 
   public default void indexerStop() {}
 }
---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems.indexer;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class IndexerIOTalonFX implements IndexerIO {
+
+  private static TalonFX indexer = new TalonFX(882);
+  private static TalonFX feeder = new TalonFX(560);
+
+  public IndexerIOTalonFX() {}
+
+  @Override
+  public void updateInputs(IndexerIOInputs inputs){
+    
+  }
+
+
+
+  @Override
+  public void feederSetVelocity(double velocity) {
+    feeder.set(velocity);
+  }
+
+  @Override
+  public void feederStop() {
+    feeder.stopMotor();
+  }
+
+  @Override
+  public void indexerSetVelocity(double velocity) {
+    indexer.set(velocity);
+  }
+
+  @Override
+  public void indexerStop() {}
+}
