8a3bc8c25aec27879243fa7648ba4e2468ac6ef5
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2026-03-03 19:12:48 -0700

    Turn vision on

--- src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -22,14 +22,18 @@ import static frc.robot.Constants.RobotDevices.*;
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
+import frc.robot.Constants.DrivebaseConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.PowerConstants;
 import frc.robot.util.PhoenixUtil;
@@ -77,6 +81,19 @@ public class IntakeIOTalonFX implements IntakeIO {
     pivotConfig.CurrentLimits.SupplyCurrentLimit = PowerConstants.kMotorPortMaxCurrent;
     pivotConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
     pivotConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    pivotConfig.Slot0 =
+        new Slot0Configs()
+            .withKP(DrivebaseConstants.kSteerP)
+            .withKI(0.0)
+            .withKD(DrivebaseConstants.kSteerD)
+            .withKS(0.0)
+            .withKV(0.0)
+            .withKA(0.0)
+            .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
+    pivotConfig.Feedback.FeedbackRemoteSensorID = pivotEncoder.getDeviceID();
+    // When not Pro-licensed, FusedCANcoder/SyncCANcoder automatically fall back to RemoteCANcoder
+    pivotConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+    pivotConfig.Feedback.RotorToSensorRatio = 25.0 * (60.0 / 18.0); // Planetary -> chain sprocket
 
     // cancoder
     cancoderConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1.0;
