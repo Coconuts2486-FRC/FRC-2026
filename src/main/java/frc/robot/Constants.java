c02cdda773629b7e2b69d19706cf3672688f996b
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2026-03-09 13:41:55 -0700

    Regression formulation ready for Regression

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -350,10 +350,10 @@ public final class Constants {
   /** Example Flywheel Mechanism Constants ********************************* */
   public static final class ShooterConstants {
 
-    public static final Translation3d shooterTranslate = new Translation3d(0.0, 0.0, 0.0);
-    public static final Rotation3d shooterRotation = new Rotation3d(0.0, 0.0, 0.0);
-    public static final Transform3d shooterTransform =
-        new Transform3d(shooterTranslate, shooterRotation);
+    public static final Transform3d kShooterTransform =
+        new Transform3d(
+            new Translation3d(Units.inchesToMeters(-5.5), 0.0, Units.inchesToMeters(20.0)),
+            Rotation3d.kZero);
 
     // Mechanism idle mode
     public static final MotorIdleMode kShooterIdleMode = MotorIdleMode.COAST; // BRAKE, COAST
