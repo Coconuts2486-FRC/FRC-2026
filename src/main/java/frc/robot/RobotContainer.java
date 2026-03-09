6fd35136e9c74743b07ab0b58c7e9f81af390c8c
OscarCDavis <oscarcdavis@gmail.com>
2026-03-09 15:45:03 -0700

    fixed changing speed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -514,10 +514,6 @@ public class RobotContainer {
 
     driverController.b().toggleOnTrue(Commands.run(() -> m_rollers.runRollers(), m_rollers));
 
-    driverController.povUp().onTrue(Commands.run(() -> m_shooter.increaseSpeed()));
-
-    driverController.povDown().onTrue(Commands.run(() -> m_shooter.decreaseSpeed()));
-
     // driverController
     //     .povUp()
     //     .whileTrue(
@@ -560,7 +556,7 @@ public class RobotContainer {
 
     driverController
         .leftTrigger()
-        .toggleOnTrue(Commands.run(() -> m_shooter.runVelocity(), m_shooter))
+        .toggleOnTrue(Commands.run(() -> m_shooter.runVelocity(12.5), m_shooter))
         // .alongWith(Commands.run(() -> m_feeder.setFeederVelocity(0.5), m_feeder)))
         .onFalse(
             Commands.run(() -> m_shooter.stop(), m_shooter)
