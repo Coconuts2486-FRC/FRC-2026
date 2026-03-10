b064380bafca4de0332342c8bc524f57a931f1a4
OscarCDavis <oscarcdavis@gmail.com>
2026-03-10 15:35:00 -0700

    added stop default function for shooter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -469,9 +469,7 @@ public class RobotContainer {
     m_shooter.setDefaultCommand(
         Commands.run(
             () -> {
-              // Listen to the Coordinator, and set the velocity based on physics computation.
-              // If
-              // in "Don't Shoot" status, set speed to some idle value.
+              m_shooter.stop();
             },
             m_shooter));
 
@@ -505,8 +503,7 @@ public class RobotContainer {
         .leftTrigger()
         .whileTrue(
             Commands.run(
-                () -> m_shooter.runVelocity(Coordinator.getShooterVelocity() - 0.15), m_shooter))
-        .onFalse(Commands.runOnce(() -> m_shooter.stop()));
+                () -> m_shooter.runVelocity(Coordinator.getShooterVelocity() - 0.15), m_shooter));
 
     // auto aim
     driverController
