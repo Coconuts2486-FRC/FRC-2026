bf86ecf08b0d99999a76e78637387f4fe3816363
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2026-03-10 10:22:19 -0700

    Okay it works now just forgot to add requirements

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -205,9 +205,9 @@ public class RobotContainer {
 
   public void defineAutoCommands() {
 
-    NamedCommands.registerCommand("IntakeDown", Commands.run(() -> m_intake.pivotDown(), m_intake).alongWith(Commands.print("NAMED COMMAND WORKED")));
+    NamedCommands.registerCommand("IntakeDown", Commands.run(() -> m_intake.pivotDown(), m_intake));
 
-    NamedCommands.registerCommand("Intake", Commands.run(() -> m_rollers.runRollers(), m_rollers).alongWith(Commands.print("NAMED COMMAND WORKED")));
+    NamedCommands.registerCommand("Intake", Commands.run(() -> m_rollers.runRollers(), m_rollers));
 
     // NamedCommands.registerCommand(
     //   "ClimbPrepare",
