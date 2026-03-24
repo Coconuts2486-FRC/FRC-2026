eef3ac3c5ef20542b7b4dd5ff57bc0134390b131
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2026-03-24 12:06:18 -0700

    Mirrored a path and made one for getting fuel from depot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -183,7 +183,7 @@ public class RobotContainer {
     NamedCommands.registerCommand(
         "Shoot",
         Commands.runOnce(
-            () -> m_shooter.set(Math.abs(Coordinator.getShooterVelocity())), m_shooter));
+            () -> m_shooter.set(Math.abs(Coordinator.getShooterVelocity())), m_shooter).finallyDo(()->m_shooter.stop()));
 
     NamedCommands.registerCommand(
         "Align",
